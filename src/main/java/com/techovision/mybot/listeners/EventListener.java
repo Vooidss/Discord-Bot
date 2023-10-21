65989c999b1c541a70b8c3a5323d090a1644694c
Egor <gegor433@yandex.ru>
2023-10-22 00:30:58 +0300

    :)

--- src/main/java/com/techovision/mybot/listeners/EventListener.java
+++ src/main/java/com/techovision/mybot/listeners/EventListener.java
@@ -2,6 +2,7 @@ package com.techovision.mybot.listeners;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
@@ -12,6 +13,7 @@ import net.dv8tion.jda.api.events.user.update.UserUpdateOnlineStatusEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Locale;
 
 public class EventListener extends ListenerAdapter {
@@ -50,23 +52,38 @@ public class EventListener extends ListenerAdapter {
 
     @Override
     public void onUserUpdateOnlineStatus(@NotNull UserUpdateOnlineStatusEvent event) {
+
         User user = event.getUser();
         OnlineStatus status = event.getMember().getOnlineStatus();
         String statusMember = "";
         String txt ="";
+        List<Member> members= event.getGuild().getMembers();
+        int CountUsersInOnline = 0;
+
+        for(Member member : members){
+            if(member.getOnlineStatus() == OnlineStatus.ONLINE)
+                CountUsersInOnline++;
+        }
 
         switch (status){
-            case ONLINE -> statusMember = "Онлай";
+            case ONLINE -> statusMember = "Онлайн";
             case DO_NOT_DISTURB -> statusMember = "Небеспокоить";
             case IDLE -> statusMember = "Отошёл";
             case OFFLINE -> statusMember ="Вышел";
         }
+
             if (statusMember != "Вышел")
                 txt = "**" + user.getName() + "** обновил свой статус на -> " + statusMember;
             else
                 txt = "**" + user.getName() + "** -> " + statusMember;
 
-            TextChannel channel = event.getGuild().getSystemChannel();
-            channel.sendMessage(txt).queue();
+            if(statusMember == "Онлайн")
+                txt += "\nТеперь участников онлайн -> " + CountUsersInOnline;
+
+
+        TextChannel channel = event.getGuild().getSystemChannel();
+        channel.sendMessage(txt).queue();
+
+
     }
 }
